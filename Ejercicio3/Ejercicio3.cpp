8b7ab964b08ce3fab2f6aadda9141362ab60e987
Lautaro Lo Presti <llopresti@udesa.edu.ar>
2025-03-14 14:09:32 -0300

    Ejercicio 3 terminado

--- Ejercicio3/Ejercicio3.cpp
+++ Ejercicio3/Ejercicio3.cpp
@@ -41,4 +41,77 @@ void push_front(shared_ptr<List> lista, shared_ptr<Node> nodo){
     lista->size++;
 }
 
+void push_back(shared_ptr<List> lista, shared_ptr<Node> nodo){
+    shared_ptr<Node> current = lista->head;
+    if(current == nullptr){
+        lista->head = nodo;
+        lista->size++;
+        return;
+    }
+    while(current->next != nullptr){
+        current = current->next;
+    }
+    current->next = nodo;
+    lista->size++;
+}
 
+void insert(shared_ptr<List> lista, shared_ptr<Node> nodo, int position){
+    if (position < 0){
+        cout << "La posicion es menor a 0, se agrega al inicio de la lista" << endl;
+        push_front(lista, nodo);
+        return;
+    }
+    if(position > lista->size){
+        cout << "La posicion es mayor al largo de la lista, se agrega al final de la lista" << endl;
+        push_back(lista, nodo);
+        return;
+    }
+    shared_ptr<Node> current = lista->head;
+    if(position == 0){
+        push_front(lista, nodo);
+        return;
+    }
+    for(int i = 0; i < position - 1; i++){
+        current = current->next;
+    }
+    nodo->next = current->next;
+    current->next = nodo;
+    lista->size++;
+}
+
+void erase(shared_ptr<List> lista, int position){
+    if(position < 0){
+        cout << "La posicion es menor a 0, se borra el primer nodo de la lista" << endl;
+        lista->head = lista->head->next;
+        lista->size--;
+        return;
+    }
+    if(position >= lista->size){
+        cout << "La posicion es mayor al largo de la lista, se borra el ultimo nodo de la lista" << endl;
+        shared_ptr<Node> current = lista->head;
+        while(current->next->next != nullptr){
+            current = current->next;
+        }
+        current->next = nullptr;
+        lista->size--;
+        return;
+    }
+    shared_ptr<Node> current = lista->head;
+    for(int i = 0; i < position - 1; i++){
+        current = current->next;
+    }
+    current->next = current->next->next;
+    lista->size--;
+}
+
+void print_list(shared_ptr<List> lista){
+    shared_ptr<Node> current = lista->head;
+    while(current != nullptr){
+        cout << current->value;
+        if(current->next != nullptr){
+            cout << " -> ";
+        }
+        current = current->next;
+    }
+    cout << endl;
+}
--- Ejercicio3/Ejercicio3.h
+++ Ejercicio3/Ejercicio3.h
@@ -17,3 +17,25 @@ Inserta un nodo al frente de la lista.
 */
 void push_front(shared_ptr<List> lista, shared_ptr<Node> nodo);
 
+/*
+Inserta un nodo al final de la lista.
+*/
+void push_back(shared_ptr<List> lista, shared_ptr<Node> nodo);
+
+/*
+Inserta un nodo en la posición que se le pase a la función. Si se le pasa 
+una posición mayor al largo de la lista, lo indica y agrega al final de la lista
+*/
+void insert(shared_ptr<List> lista, shared_ptr<Node> nodo, int position);
+
+/*
+Borra  un  nodo  en  la  posición  que  se  le  pase  a  la  función.  Similar  a  la 
+función insert(), si la posición es mayor que el largo de la lista, se debe de borrar 
+el último nodo. 
+*/
+void erase(shared_ptr<List> lista, int position);
+
+/*
+Imprime la lista completa, separando el valor en cada nodo con “->”. 
+*/
+void print_list(shared_ptr<List> lista);
